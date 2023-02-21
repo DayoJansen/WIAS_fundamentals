412687c4e27224378bd7ed1a9b15adc1b3223bf6
cwtyson <cwtyson@ucdavis.edu>
2023-02-21 07:58:08 +0100

    Add example exercises as backup

---
+++ misc/backup_activity/example_data_script.R
@@ -0,0 +1,14 @@
+## Experimentn with example data
+
+
+## Read in data
+ex_data <- readRDS("./misc/backup_activity/example_data.RDS")
+
+
+## Things to do with the data
+# 1. What is the usual interval between detections?
+# 2. Are there daily patterns in the detection times?
+# 3. Calculate the distance between detections. What could you explore in relation to the distance traveled?
+# 4. Make a map of the detections. Add satellite imagery to this map. 
+# 5. Try other visualizations from yesterday's tutorial, such as animations.
+# 6. Use other tools from yesterday 
